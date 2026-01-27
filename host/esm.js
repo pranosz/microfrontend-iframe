90baccafe7a366fe7e9a971195a84e27bc76fdab
Piotr Ranosz <pranosz@wp.pl>
2026-01-27 22:25:45 +0100

    esm

---
+++ host/esm.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+module.exports = (env = {}) => {
+  const isStandalone = env.standalone;
+
+  return {
+    mode: "production",
+
+    entry: isStandalone
+      ? "./src/standalone.tsx"
+      : "./src/cto-platform-support.tsx",
+
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "cto-platform-support.js",
+
+      // 🔥 KLUCZOWE
+      library: {
+        type: "module",
+      },
+
+      clean: true,
+      publicPath: "",
+    },
+
+    experiments: {
+      outputModule: true, // 🔥 BEZ TEGO NIE MA ESM
+    },
+
+    externals: {
+      react: "react",
+      "react-dom": "react-dom",
+      "single-spa": "single-spa",
+    },
+
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
+};
